3843897eb9c77861c4e76ea9636ebe2d6981d39a
taura.kenjiro@gmail.com <taura.kenjiro@gmail.com@87c84bbb-601f-3839-02e0-633ee11d346b>
2013-02-06 13:27:32 +0000

    not to download gxp everytime

--- trunk/master_scripts/S10gxp/S10gxp.mk
+++ trunk/master_scripts/S10gxp/S10gxp.mk
@@ -1,3 +1,6 @@
-all :
+all : ok
+
+ok :
 	cvs -d:pserver:anonymous:@gxp.cvs.sourceforge.net:/cvsroot/gxp login
 	cvs -z3 -d:pserver:anonymous@gxp.cvs.sourceforge.net:/cvsroot/gxp co -P gxp3
+	touch $@
