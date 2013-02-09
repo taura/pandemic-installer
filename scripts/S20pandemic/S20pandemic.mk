17ef9a81a8e309c81f92b95d87c7d30011343c5d
taura.kenjiro <taura.kenjiro@gmail.com>
2013-02-09 16:20:42 +0000

    merge download svn into S20pandemic

--- scripts/S20pandemic/S20pandemic.mk
+++ scripts/S20pandemic/S20pandemic.mk
@@ -5,7 +5,10 @@
 # so that we can just run become_pandemic_master
 #
 
+pandemic_packages := isc-dhcp-server tftpd-hpa nfs-kernel-server syslinux 
 all :
+# download tools we need in the master, but not install
+	chroot $(custom_live_squashfs_root) apt-get --force-yes -y --download-only install $(pandemic_packages)
+	chroot $(custom_live_squashfs_root) apt-get --force-yes -y install subversion
 	chroot $(custom_live_squashfs_root) rm -rf /root/pi
 	chroot $(custom_live_squashfs_root) svn checkout http://pandemic-installer.googlecode.com/svn/trunk /root/pi
-	chroot $(custom_live_squashfs_root) apt-get --download-only --force-yes -y install isc-dhcp-server tftpd-hpa nfs-kernel-server syslinux
