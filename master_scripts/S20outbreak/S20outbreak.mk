4a259f6439edfbf4aced0ed7b0cabfa5eb601b3a
nakashima <nakashima@logos.ic.i.u-tokyo.ac.jp>
2013-02-11 11:33:43 +0000

    add script for downloading packages to run outbreak

--- master_scripts/S20outbreak/S20outbreak.mk
+++ master_scripts/S20outbreak/S20outbreak.mk
@@ -1,7 +1,15 @@
-all: sonic
+#
+# S20outbreak.mk
+#
+# prepare to run outbreak
+#
 
 CFLAGS = -Wall -O3 -g -static
 LDFLAGS = -lpthread
+
+all: sonic
+	chmod +x outbreak
+	apt-get -y install realpath
 
 sonic: sonic.c
 	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS)
