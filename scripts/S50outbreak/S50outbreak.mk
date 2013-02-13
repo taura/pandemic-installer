a19bd67bd4e5dde6a431d5a01e2088b953d6bc7d
nakashima <nakashima@logos.ic.i.u-tokyo.ac.jp>
2013-02-13 05:36:48 +0000

    add one-path script to do pandemic install

--- scripts/S50outbreak/S50outbreak.mk
+++ scripts/S50outbreak/S50outbreak.mk
@@ -8,4 +8,5 @@ outbreak_packages := realpath
 
 all :
 	chroot $(custom_live_squashfs_root) apt-get --force-yes -y --download-only --force-yes -y install $(outbreak_packages)
+	cp outbreak $(custom_live_squashfs_root)/root/
 
