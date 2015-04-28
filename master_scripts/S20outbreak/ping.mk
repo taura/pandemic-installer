623327c5508859371e2909643e596ae6585f77e7
taura.kenjiro <taura.kenjiro@gmail.com>
2015-04-28 05:38:22 +0000

    ping client addresses before explore + fix a typo in outbreak2

---
+++ master_scripts/S20outbreak/ping.mk
@@ -0,0 +1,17 @@
+address_prefix=10.0.3.
+last_octet_begin=100
+last_octet_end=102
+
+out_dir:=/tmp/$(USER)/$(shell date +%Y-%m-%d-%H-%M-%S-%N)/ping_result
+last_octets:=$(shell seq $(last_octet_begin) $(last_octet_end))
+all_pings:=$(addprefix $(out_dir)/$(address_prefix),$(last_octets))
+
+all : $(all_pings)
+	ls -1 $(out_dir)/
+
+$(all_pings) : $(out_dir)/% : $(out_dir)
+	if ping -c 1 -W 1 $* > /dev/null; then touch $@ ; fi
+
+$(out_dir) : 
+	mkdir -p $@
+
