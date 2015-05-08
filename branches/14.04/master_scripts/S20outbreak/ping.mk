18c5fff2297b785633c907d05bcaa260a3994d53
taura.kenjiro@gmail.com <taura.kenjiro@gmail.com@87c84bbb-601f-3839-02e0-633ee11d346b>
2015-05-08 09:03:16 +0000

    added command line options to outbreak2; make it more fault tolerant; refactor outbreak2

--- branches/14.04/master_scripts/S20outbreak/ping.mk
+++ branches/14.04/master_scripts/S20outbreak/ping.mk
@@ -1,3 +1,10 @@
+#
+# a Makefile that pings clients of the specified
+# address range.  if a client responds, it creates
+# a file under /tmp/USER/TIMESTAMP/ping_result/ADDRESS,
+# and show their addresses
+#
+
 address_prefix=10.0.3.
 last_octet_begin=100
 last_octet_end=199
@@ -6,6 +13,11 @@ out_dir:=/tmp/$(USER)/$(shell date +%Y-%m-%d-%H-%M-%S-%N)/ping_result
 last_octets:=$(shell seq $(last_octet_begin) $(last_octet_end))
 all_pings:=$(addprefix $(out_dir)/$(address_prefix),$(last_octets))
 
+# all_pings is a space separated list of IP addresses to ping.
+# e.g., 10.0.3.100 10.0.3.101 10.0.3.102 ... 10.0.3.199
+#
+
+# show their addresses
 all : $(all_pings)
 	ls -1 $(out_dir)/
 
