0f202f445fc0a101ccdc4244202b8a801c89a69d
tau@eidos.ic.i.u-tokyo.ac.jp <tau@logos.t.u-tokyo.ac.jp>
2016-03-31 20:44:52 +0900

    --client option to make client diagnosis easier

--- master_scripts/S20outbreak/ping.mk
+++ master_scripts/S20outbreak/ping.mk
@@ -10,7 +10,7 @@ last_octet_begin=100
 last_octet_end=199
 
 out_dir:=/tmp/$(USER)/$(shell date +%Y-%m-%d-%H-%M-%S-%N)/ping_result
-last_octets:=$(shell seq $(last_octet_begin) $(last_octet_end))
+#last_octets:=$(shell seq $(last_octet_begin) $(last_octet_end))
 all_pings:=$(addprefix $(out_dir)/$(address_prefix),$(last_octets))
 
 # all_pings is a space separated list of IP addresses to ping.
