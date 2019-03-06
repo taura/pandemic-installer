2e7cd3bf1d01694b5cfc10021097ef372c185a1e
tau@eidos.ic.i.u-tokyo.ac.jp <tau@logos.t.u-tokyo.ac.jp>
2019-03-06 18:55:09 +0900

    realpath -> coreutils

--- scripts/S50outbreak/S50outbreak.mk
+++ scripts/S50outbreak/S50outbreak.mk
@@ -4,9 +4,13 @@
 # pre-download essential packages needed to run outbreak
 #
 
-outbreak_packages := realpath
+# 16.04 or earlier
+#outbreak_packages := realpath
+# 18.04 or earlier
+outbreak_packages := coreutils
 
 all :
-	chroot $(custom_live_squashfs_root) apt-get --force-yes -y --download-only --force-yes -y install $(outbreak_packages)
+#	chroot $(custom_live_squashfs_root) apt-get --force-yes -y --download-only install $(outbreak_packages)
+	chroot $(custom_live_squashfs_root) apt-get -y --download-only install $(outbreak_packages)
 	cp outbreak $(custom_live_squashfs_root)/root/
 
