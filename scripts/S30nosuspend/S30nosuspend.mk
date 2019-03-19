f8b4902d92c11454d0904f8b3ee1dbc8ab389387
tau@eidos.ic.i.u-tokyo.ac.jp <tau@logos.t.u-tokyo.ac.jp>
2019-03-20 00:12:13 +0900

    no suspend 18.04

--- scripts/S30nosuspend/S30nosuspend.mk
+++ scripts/S30nosuspend/S30nosuspend.mk
@@ -5,5 +5,10 @@
 #
 
 all :
-	cp 10_disable-suspend.gschema.override $(custom_live_squashfs_root)/usr/share/glib-2.0/schemas/
-	cp 10_disable-suspend.gschema.override $(custom_live_squashfs_patient_root)/usr/share/glib-2.0/schemas/
+# old ubuntu
+#	cp 10_disable-suspend.gschema.override $(custom_live_squashfs_root)/usr/share/glib-2.0/schemas/
+#	cp 10_disable-suspend.gschema.override $(custom_live_squashfs_patient_root)/usr/share/glib-2.0/schemas/
+	echo 'HandleLidSwitch=ignore'       | tee --append $(custom_live_squashfs_root)/etc/systemd/logind.conf
+	echo 'HandleLidSwitchDocked=ignore' | tee --append $(custom_live_squashfs_root)/etc/systemd/logind.conf
+	echo 'HandleLidSwitch=ignore'       | tee --append $(custom_live_squashfs_patient_root)/etc/systemd/logind.conf
+	echo 'HandleLidSwitchDocked=ignore' | tee --append $(custom_live_squashfs_patient_root)/etc/systemd/logind.conf
